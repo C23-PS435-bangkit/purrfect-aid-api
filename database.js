f32369989d42609a516f4754bed6d8c812f64223
NdnHnnt <nadinehaninta@gmail.com>
2023-05-24 08:42:51 +0700

    #2 Add Environment

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
+require('dotenv').config;
 const mysql = require('mysql2');
 
 // SQL Placeholder, nanti bisa diganti pakai CloudSQL
 const pool = mysql.createPool({
-  host: 'sql12.freemysqlhosting.net',
-  user: 'sql12620347',
-  password: 'GDKQFSyLXl',
-  database: 'sql12620347',
+  host: process.env.dbhost,
+  user: process.env.dbuser,
+  database: process.env.dbdatabase, 
+  password: process.env.dbpassword,
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
